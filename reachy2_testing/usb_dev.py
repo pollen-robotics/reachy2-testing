7c8291266e5a20cc7b974f1de104a8075c71d4c0
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2025-08-20 11:57:17 +0200

    Typo in lidar udev rule check.

--- reachy2_testing/usb_dev.py
+++ reachy2_testing/usb_dev.py
@@ -4,17 +4,17 @@ import openhtf as htf
 
 
 @htf.measures(
-    htf.Measurement("is_rplidar2_dev_exists")
+    htf.Measurement("is_rplidar_s2_dev_exists")
     .equals(True)
 )
-def rplidar2_dev(test):
-    test.logger.info("checking /dev/rplidar2")
-    test.measurements.is_rplidar2_dev_exists= os.path.exists("/dev/rplidar2")
-    if test.measurements.is_rplidar2_dev_exists:
-        test.logger.info("/dev/rplidar2 exists")
+def rplidar_s2_dev(test):
+    test.logger.info("checking /dev/rplidar_s2")
+    test.measurements.is_rplidar_s2_dev_exists= os.path.exists("/dev/rplidar_s2")
+    if test.measurements.is_rplidar_s2_dev_exists:
+        test.logger.info("/dev/rplidar_s2 exists")
         return htf.PhaseResult.CONTINUE
     else:
-        test.logger.error("/dev/rplidar2 not found!")
+        test.logger.error("/dev/rplidar_s2 not found!")
         return htf.PhaseResult.FAIL_AND_CONTINUE
 
 @htf.measures(
